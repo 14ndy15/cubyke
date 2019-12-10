9ebec9214fa0b5a483b9a524038140aa7b782634
landy <14ndy15@gmail.com>
2019-12-10 16:07:51 -0500

    bug fix hero margin and add null to positionDe in team member

--- src/Entity/TeamMember.php
+++ src/Entity/TeamMember.php
@@ -55,7 +55,7 @@ class TeamMember extends ImageField
     private $positionEs;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $positionDe;
 
