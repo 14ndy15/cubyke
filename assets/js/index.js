e412a5394c4e812cb5f60e3ab7acc05144afb155
landy <14ndy15@gmail.com>
2019-12-13 15:16:50 -0500

    review section on tours

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
 import '../css/index.scss';
+import 'lazysizes/plugins/unveilhooks/ls.unveilhooks.min';
 
 import Gallery from "./modules/global/_gallery";
 
 new Gallery('.bikes__gallery');
+console.log('dsad');
