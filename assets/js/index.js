77d0635b36fd4e4a4e2b5434d3e886bc8b431931
landy <14ndy15@gmail.com>
2019-12-13 16:36:03 -0500

    build

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,5 @@
 import '../css/index.scss';
 import 'lazysizes/plugins/unveilhooks/ls.unveilhooks.min';
-
 import Gallery from "./modules/global/_gallery";
 
 new Gallery('.bikes__gallery');
-console.log('dsad');
