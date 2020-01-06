c85cc03007f876ae3c8a172d033285e1aa78fc8a
landy <14ndy15@gmail.com>
2020-01-06 17:17:06 -0500

    webp convert file

--- public/make_webp.php
+++ public/make_webp.php
@@ -16,10 +16,16 @@ function makeWebP(){
                     continue;
                 }
 
-                $command = $script.' '.$workingFolder.$file.' -quality 85 '.$workingFolder.$file.'.webp';
+                $ext = pathinfo($workingFolder.$file, PATHINFO_EXTENSION);
+                if ($ext == 'jpeg' || $ext == 'jpg' || $ext == 'png') {
+
+                    $command = $script . ' ' . $workingFolder . $file . ' -quality 85 ' . $workingFolder . $file . '.webp';
 //                    $command = $script.' -q 85 '.$workingFolder.$file.' -o '.$workingFolder.$file.'.webp';
-                echo $command.'<br>';
-                exec($command);
+                    echo $command . '<br>';
+                    exit();
+                    exec($command);
+                }
+
             }
     }
     closedir($dir);
@@ -28,4 +34,31 @@ function makeWebP(){
     exit();
 }
 
-makeWebP();
+function deleteWebP(){
+    $folders = ['media/'];
+    $files = [];
+    for($i = 0; $i < sizeof($folders); $i++) {
+        $workingFolder= $folders[$i];
+        if ($dir = opendir($workingFolder))
+            while (false !== ($file = readdir($dir))) {
+
+                if ($file === '.' || $file === '..')
+                    continue;
+
+                if (is_dir($workingFolder.$file)) {
+                    array_push($folders, $workingFolder . $file . '/');
+                    continue;
+                }
+
+                $ext = pathinfo($workingFolder.$file, PATHINFO_EXTENSION);
+                if ($ext == 'wepb')
+                    unlink($workingFolder . $file);
+            }
+    }
+    closedir($dir);
+
+    print_r($files);
+    exit();
+}
+
+deleteWebP();
