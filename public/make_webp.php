d335ebd3bcd6a5b7f702fa65fd28c7ab1a05ba87
landy <14ndy15@gmail.com>
2020-01-07 15:05:30 -0500

    working webp script

--- public/make_webp.php
+++ public/make_webp.php
@@ -1,7 +1,10 @@
 <?php
 function makeWebP(){
     $script = 'convert';
-    $folders = ['media/'];
+    $folders = ['uploads/', 'images/'];
+    $outputFolder = 'media/cache/';
+    $resolutions = [1920, 1200, 1000, 900, 800, 600];
+
     $files = [];
     for($i = 0; $i < sizeof($folders); $i++) {
         $workingFolder= $folders[$i];
@@ -11,25 +14,33 @@ function makeWebP(){
                 if ($file === '.' || $file === '..')
                     continue;
 
-                if (is_dir($workingFolder.$file)) {
-                    array_push($folders, $workingFolder . $file . '/');
+                $filename = $workingFolder.$file;
+
+                if (is_dir($filename)) {
+                    array_push($folders, $filename . '/');
                     continue;
                 }
 
-                $ext = pathinfo($workingFolder.$file, PATHINFO_EXTENSION);
+                $ext = pathinfo($filename, PATHINFO_EXTENSION);
                 if ($ext == 'jpeg' || $ext == 'jpg' || $ext == 'png') {
 
-                    $command = $script . ' ' . $workingFolder . $file . ' -quality 85 ' . $workingFolder . $file . '.webp';
-//                    $command = $script.' -q 85 '.$workingFolder.$file.' -o '.$workingFolder.$file.'.webp';
-                    echo $command . '<br>';
-                    exit();
-                    exec($command);
+                    $size = getimagesize($filename);
+
+                    foreach ($resolutions as $resolution) {
+
+                        if ($size[0] < $resolution)
+                            continue;
+
+                        $outputFile = __DIR__ . '/' . $outputFolder . 'min_width_' .$resolution.'/'.$filename.'.webp';
+                        $command = $script . ' ' . __DIR__. '/' . $filename . ' -quality 85  -resize '. $resolution . ' ' . $outputFile;
+                        echo $command . '<br><br>';
+//                        exec($command);
+                    }
                 }
 
             }
     }
 
-    print_r($files);
     exit();
 }
 
